b46ac16f9beddedf1e593238f251acf2be0e5459
Danilosic <danilosic.oliveira@gmail.com>
2024-05-26 03:39:03 -0300

    Variaveis e migrando statefile para iniciar exemplo da azure vmss

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+resource "azurerm_resource_group" "grupo_recursos" {
+  name     = "example-${var.location}-resources"
+  location = var.location
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_virtual_network" "rede" {
+  name                = "example-${var.location}-network"
+  resource_group_name = azurerm_resource_group.grupo_recursos.name
+  location            = azurerm_resource_group.grupo_recursos.location
   address_space       = ["10.0.0.0/16"]
 }
--- provider.tf
+++ provider.tf
@@ -2,11 +2,11 @@
 # Azure Provider source and version being used
 terraform {
   backend "azurerm" {
-    resource_group_name  = "StorageAccount-ResourceGroup"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "descomplicandoterraform"       # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "descomplicando-terraform-2024" # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "file.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-    use_oidc             = true                            # Can also be set via `ARM_USE_OIDC` environment variable.
+    resource_group_name  = "StorageAccount-ResourceGroup"     # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "descomplicandoterraform"          # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "descomplicando-terraform-2024"    # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "descomplicando.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    use_oidc             = true                               # Can also be set via `ARM_USE_OIDC` environment variable.
 }
 
   required_providers {
